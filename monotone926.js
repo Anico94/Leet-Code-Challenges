41d261aa277ef53fe11434b8c7c624b6a1d1d34a
Alex Nico <a.nicolaidis@outlook.com>
2023-01-17 21:56:27 +1000

    Completed Solution to 926

--- monotone926.js
+++ monotone926.js
@@ -1,39 +1,67 @@
-var minFlipsMonoIncr = function (s) {
-  //count the number of 1's and 0's in the string
+// var minFlipsMonoIncr = function (s) {
+//   //count the number of 1's and 0's in the string
 
+//   const totalOnes = Array.from(s).filter((num) => num === "1").length;
+//   const totalZeros = s.length - totalOnes;
+
+//   let flips0 = 0;
+//   let flips1 = 0;
+
+//   let ones = totalOnes;
+//   let zeros = totalZeros;
+//   let string = s;
+
+//   while (s.length > 0 && ones !== 0) {
+//     const currentLast = s[s.length - 1];
+
+//     if (currentLast === "0") {
+//       flips1++;
+//     } else {
+//       ones--;
+//     }
+//     s = s.slice(0, s.length - 1);
+//   }
+
+//   while (string.length > 0 && zeros !== 0) {
+//     const currentFirst = string[0];
+
+//     if (currentFirst === "1") {
+//       flips0++;
+//     } else {
+//       zeros--;
+//     }
+//     string = string.slice(1);
+//   }
+
+//   return Math.min(flips1, flips0, totalOnes, totalZeros);
+// };
+
+// console.log(minFlipsMonoIncr("10011111110010111011"));
+
+var minFlipsMonoIncr = function (s) {
   const totalOnes = Array.from(s).filter((num) => num === "1").length;
   const totalZeros = s.length - totalOnes;
+  let currentLowest = Math.min(totalOnes, totalZeros);
 
-  let flips0 = 0;
-  let flips1 = 0;
+  let currentLeftOnes = 0;
+  let currentLeftZeros = 0;
 
-  let ones = totalOnes;
-  let zeros = totalZeros;
-  let string = s;
-
-  while (s.length > 0 && ones !== 0) {
-    const currentLast = s[s.length - 1];
-
-    if (currentLast === "0") {
-      flips1++;
-    } else {
-      ones--;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "0") {
+      currentLeftZeros++;
+      continue;
     }
-    s = s.slice(0, s.length - 1);
-  }
+    const leftOnes = currentLeftOnes;
+    const rightZeros = totalZeros - currentLeftZeros;
+    currentLeftOnes++;
 
-  while (string.length > 0 && zeros !== 0) {
-    const currentFirst = string[0];
+    console.log(leftOnes, rightZeros);
 
-    if (currentFirst === "1") {
-      flips0++;
-    } else {
-      zeros--;
+    const lowest = leftOnes + rightZeros;
+
+    if (lowest < currentLowest) {
+      currentLowest = lowest;
     }
-    string = string.slice(1);
   }
-
-  return Math.min(flips1, flips0, totalOnes, totalZeros);
+  return Math.min(currentLowest);
 };
-
-console.log(minFlipsMonoIncr("0101100011"));
